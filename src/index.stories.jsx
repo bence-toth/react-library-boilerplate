8db8c6b420957c33c80ffaeb71c356d7dd165241
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-02-25 12:32:25 +0100

    Updated ESLint config and removed disable rules

--- src/index.stories.jsx
+++ src/index.stories.jsx
@@ -1,7 +1,3 @@
-/* eslint-disable import/no-unused-modules */
-// TODO: See if this ↑↑↑ can be configured
-// to be ignored in Storybook story  files
-
 import React from 'react'
 
 import HelloWorld from './index'
