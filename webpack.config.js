fa55a446475e33b28f7b26d952e9fa68a811bd94
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-02-25 12:29:32 +0100

    Added ESLint overrides for webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,3 @@
-/* eslint-disable fp/no-mutation */
-/* eslint-disable import/no-unused-modules */
-// TODO: See if these ↑↑↑ can be configured
-// to be ignored in the webpack config
-
 module.exports = {
   module: {
     rules: [
