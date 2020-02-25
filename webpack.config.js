1d76a130a4d4bf4401b10fe18aead5d436b1e094
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-02-25 12:25:42 +0100

    Documented Webpack config, added index.jsx support

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,8 @@ module.exports = {
   module: {
     rules: [
       {
+        // Process all js and jsx
+        // files with Babel
         test: /\.(js|jsx)$/,
         exclude: [/node_modules/],
         use: {
@@ -14,12 +16,20 @@ module.exports = {
         }
       },
       {
+        // Load all css imports
+        // with style-loader and css-loader
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
       }
     ]
   },
+  resolve: {
+    // Look for index.js first,
+    // then fall back to index.jsx
+    extensions: ['.js', '.jsx']
+  },
   output: {
+    // Main module will have a default export
     libraryTarget: 'umd'
   }
 }
