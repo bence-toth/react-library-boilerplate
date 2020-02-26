895081b33016a05d041b581ef570de8005025baf
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-02-26 10:28:02 +0100

    Updated test to use react-testing-library

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,16 @@
+import {render, screen} from '@testing-library/react'
+import React from 'react'
+
 import HelloWorld from './index'
 
+// eslint-disable-next-line import/no-unassigned-import
+import '@testing-library/jest-dom'
+
 describe('Test suit', () => {
-  it('Should be a function', () => {
+  it('Should say "Hello World"', () => {
+    // eslint-disable-next-line react/jsx-filename-extension
+    render(<HelloWorld />)
     expect(typeof HelloWorld).toBe('function')
+    expect(screen.getByText('Hello World')).toBeInTheDocument()
   })
 })
