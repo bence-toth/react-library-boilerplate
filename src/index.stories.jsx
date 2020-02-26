810e1e0a436cd4af6e910601059deac0b98dd8d6
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-02-26 16:55:04 +0100

    Set up knob

--- src/index.stories.jsx
+++ src/index.stories.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import {text, withKnobs} from '@storybook/addon-knobs'
 
 import HelloWorld from './index'
 
 export default {
   title: 'Hello World',
-  component: HelloWorld
+  component: HelloWorld,
+  decorators: [withKnobs]
 }
 
 export const Default = () => (
-  <HelloWorld />
+  <HelloWorld
+    name={text('Name', 'John Doe')}
+  />
 )
