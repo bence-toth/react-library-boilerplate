c0e3252e8d43cf6a011ac9f99c828e4fa509020a
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-02-27 17:52:38 +0100

    Undid some changes

--- storyshots.puppeteer.test.js
+++ storyshots.puppeteer.test.js
@@ -1,20 +1,11 @@
 import initStoryshots from '@storybook/addon-storyshots'
 import {imageSnapshot} from '@storybook/addon-storyshots-puppeteer'
 
-const storybookUrl = (
-  (process.env.CI === 'true')
-    ? `file://${process.cwd()}/storybook-dist`
-    : 'http://localhost:6006/'
-)
-
-const getScreenshotOptions = () => ({
-  fullPage: false
-})
-
-initStoryshots({
-  suite: 'Visual snapshots',
-  test: imageSnapshot({
-    storybookUrl,
-    getScreenshotOptions
+if (process.env.CI !== 'true') {
+  initStoryshots({
+    suite: 'Visual snapshots',
+    test: imageSnapshot({
+      storybookUrl: 'http://localhost:6006/'
+    })
   })
-})
+}
