43509a9deda0378d2b437b09bd6d8c52dae74177
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-02-27 14:11:12 +0100

    Using static Storybook build when in CI

--- storyshots.puppeteer.test.js
+++ storyshots.puppeteer.test.js
@@ -1,9 +1,15 @@
 import initStoryshots from '@storybook/addon-storyshots'
 import {imageSnapshot} from '@storybook/addon-storyshots-puppeteer'
 
+const storybookUrl = (
+  (process.env.CI === 'true')
+    ? `file://${__dirname}/storybook-dist`
+    : 'http://localhost:6006/'
+)
+
 initStoryshots({
   suite: 'Visual snapshots',
   test: imageSnapshot({
-    storybookUrl: 'http://localhost:6006/'
+    storybookUrl
   })
 })
