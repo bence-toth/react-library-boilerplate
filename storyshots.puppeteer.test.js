33e09301555bec15d3770cd152cc77e4b38a1628
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-03-09 12:27:29 +0100

    Added comments to visual test setup

--- storyshots.puppeteer.test.js
+++ storyshots.puppeteer.test.js
@@ -3,6 +3,9 @@ import {imageSnapshot} from '@storybook/addon-storyshots-puppeteer'
 
 if (process.env.CI !== 'true') {
   initStoryshots({
+    // Setting up visual snapshots.
+    // Read more:
+    // https://storybook.js.org/docs/testing/automated-visual-testing/
     suite: 'Visual snapshots',
     test: imageSnapshot({
       storybookUrl: 'http://localhost:6006/'
@@ -11,6 +14,10 @@ if (process.env.CI !== 'true') {
 }
 else {
   describe('Visual snapshots', () => {
+    // Preventing visual snapshots from running in CI.
+    // This is due to:
+    // - https://storybook.js.org/docs/testing/automated-visual-testing/#challenges
+    // - https://github.com/bence-toth/react-library-boilerplate/issues/51
     it('are disabled due to font smoothing issues', () => {
       expect(true).toBe(true)
     })
