1383459abd087505833d6a0d62f08c18910605a6
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-03-09 12:27:39 +0100

    Added comments to structural test setup

--- storyshots.test.js
+++ storyshots.test.js
@@ -1,5 +1,8 @@
 import initStoryshots from '@storybook/addon-storyshots'
 
 initStoryshots({
+  // Setting up structural snapshots.
+  // Read more:
+  // https://storybook.js.org/docs/testing/structural-testing/
   suite: 'Structural snapshots'
 })
