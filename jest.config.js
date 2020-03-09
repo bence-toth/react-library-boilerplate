bc4b7102573a72e1dd8aac8c5db6dcf2ed558a90
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-03-09 12:26:39 +0100

    Added comments to Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
+// This is the configuration file for Jest.
+// Read more:
+// https://jestjs.io/docs/en/configuration
+
 module.exports = {
   moduleNameMapper: {
+    // Mocking style imports for Jest (do nothing).
+    // This will allow Jest not to crash on CSS imports.
     '\\.css$': '<rootDir>/.jest/mocks/styleMock.js'
   }
 }
