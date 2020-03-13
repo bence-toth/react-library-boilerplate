05d7904a7f56cbf0e6e83cb927ca82170866c613
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-03-13 13:19:11 +0100

    Switched to react testing library for storyshots

--- storyshots.test.js
+++ storyshots.test.js
@@ -1,8 +1,20 @@
 import initStoryshots from '@storybook/addon-storyshots'
+import {render} from '@testing-library/react'
+
+const reactTestingLibrarySerializer = {
+  print: (value, serialize) => serialize(value.container.firstChild),
+  // eslint-disable-next-line no-prototype-builtins
+  test: value => value?.hasOwnProperty('container')
+}
 
 initStoryshots({
   // Setting up structural snapshots.
   // Read more:
   // https://storybook.js.org/docs/testing/structural-testing/
-  suite: 'Structural snapshots'
+  suite: 'Structural snapshots',
+  framework: 'react',
+  renderer: render,
+  snapshotSerializers: [
+    reactTestingLibrarySerializer
+  ]
 })
