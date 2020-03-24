f08b7c7b7679a7ce592f52e27c28f4b631a2009c
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-03-24 17:18:15 +0100

    Restricted code coverage collection to src folder

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@
 // https://jestjs.io/docs/en/configuration
 
 module.exports = {
+  collectCoverageFrom: [
+    'src/**/*.{ts,js,jsx}'
+  ],
   moduleNameMapper: {
     // Mocking style imports for Jest (do nothing).
     // This will allow Jest not to crash on CSS imports.
